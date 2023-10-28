bae3d4f37d5d09a9d9c5b04dccf8711b42518844
tinusaran <114895238+tinusaran@users.noreply.github.com>
2023-10-28 22:31:52 +0530

    Add files via upload

---
+++ asst1_ques3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class asst1_ques3
+{
+public static void main(String arg[])
+{
+int a;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter a Value : ");
+a=s.nextInt();
+if(a%3==0){
+System.out.println("True");}
+else{
+System.out.println("False");}
+}
+}
