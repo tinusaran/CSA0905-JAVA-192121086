55cfd38b56edfad5361bc221e50de6ac1bb7d410
tinusaran <114895238+tinusaran@users.noreply.github.com>
2023-11-27 08:22:30 +0530

    Add files via upload

---
+++ subtraction.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class subtraction
+{
+public static void main(String arg[]);
+{
+int a,b,c;
+Scanner s=new Scanner(System.in);
+System.out.println("enter a value : ");
+a=s.nextInt();
+System.out.println("enter b value : ");
+b=s.nextInt();
+c=a-b;
+System.out.println("result:" +c);
+}
+}
