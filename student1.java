ba539614e1503de635ddb78b2fea8fe383e74cb3
tinusaran <114895238+tinusaran@users.noreply.github.com>
2023-11-27 08:23:41 +0530

    Add files via upload

---
+++ student1.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class student1{
+String name;
+int regNo,total=0;
+double average=0;
+int marks[]=new int[5];
+student1(String a,int b,int c[]){
+name=a;
+regNo=b;
+marks=c;
+}
+void calculate(){
+int i;
+for(i=0;i<5;i++){
+total=total+marks[i];
+}
+average=total/5;
+}
+void display(){
+System.out.println("Name of the Student : "+name);
+System.out.println("Register Number : "+regNo);
+System.out.println("Total Marks :"+total);
+System.out.println("Average Marks : "+average);
+}
+public static void main(String arg[]){
+String x;
+int y,i;
+int z[]=new int[5];
+Scanner s=new Scanner(System.in);
+System.out.print("Enter Name : ");
+x=s.nextLine();
+System.out.print("Enter Register Number : ");
+y=s.nextInt();
+for(i=0;i<5;i++){
+System.out.print("Enter Mark "+(i+1)+":");
+z[i]=s.nextInt();
+}
+student1 obj=new student1(x,y,z);
+obj.calculate();
+obj.display();
+}
+}
+ 
