b002b55a38fb632c155a81a25ebdabb493a45beb
tinusaran <114895238+tinusaran@users.noreply.github.com>
2023-11-29 13:58:10 +0530

    Add files via upload

---
+++ RightTriangleNumberPattern.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class RightTriangleNumberPattern {
+public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+System.out.print("Enter the value of n: ");
+int n = s.nextInt();
+for (int i = 1; i <= n; i++) {
+for (int j = 1; j <= i; j++) {
+System.out.print(i+" ");
+}
+System.out.println();}
+}
+}
