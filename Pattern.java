8650323a29e322464a598a953d46979fb0ffb2a0
tinusaran <114895238+tinusaran@users.noreply.github.com>
2023-11-29 13:57:31 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,30 @@
+public class Pattern {
+    public static void main(String[] args) {
+        int n = 4; // Change this value to adjust the size of the pattern
+        // Print the top half of the pattern
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("   ");
+            }
+            // Print the numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + "  ");
+            }
+            System.out.println();
+        }
+
+        // Print the bottom half of the pattern
+        for (int i = n - 1; i >= 1; i--) {
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("   ");
+            }
+            // Print the numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
