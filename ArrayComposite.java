f277cb7c68182b371311d7485961f04fc280d0af
tinusaran <114895238+tinusaran@users.noreply.github.com>
2023-12-02 01:38:52 +0530

    Add files via upload

--- ArrayComposite.java
+++ ArrayComposite.java
@@ -3,7 +3,7 @@ class ArrayComposite{
 public static void main(String arg[]){
 Scanner s=new Scanner(System.in);
 int size,i,j;
-int count=0;
+int C_count=0,P_count=0;
 System.out.print("Enter the size of array : ");
 size=s.nextInt();
 int[] arr=new int[size];
@@ -11,7 +11,6 @@ System.out.print("Enter the Array Elements :");
 for(i=0;i<size;i++){
 arr[i]=s.nextInt();
 }
-System.out.println("Composite Numbers in Array : ");
 for(i=0;i<size;i++){
 int flag=0;
 for(j=1;j<=arr[i];j++){
@@ -19,9 +18,11 @@ if(arr[i]%j==0){
 flag++;}
 }
 if(flag>2){
-count++;
-System.out.println(arr[i]+" ");}
+C_count++;}
+else if(flag==2){
+P_count++;}
 }
-System.out.print("Number of Composite numbers in array = "+count);
+System.out.println("Number of Prime numbers in array = "+P_count);
+System.out.println("Number of Composite numbers in array = "+C_count);
 }
 }
