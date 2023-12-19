3725816a17f4afae5e69cca193b5f2c16538e73c
tinusaran <114895238+tinusaran@users.noreply.github.com>
2023-12-19 22:50:46 +0530

    Add files via upload

---
+++ pracsqcube.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class pracsqcube
+{
+public static void main(String arg[])
+{
+Scanner s=new Scanner(System.in);
+int n,sq,cb;
+System.out.println("enter the number:");
+n=s.nextInt();
+sq=n*n;
+System.out.println("sq of number is:" +sq);
+cb=n*n*n;
+System.out.println("cube of number is:" +cb);
+}
+}
