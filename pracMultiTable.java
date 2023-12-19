1228c4d20d63cc1c63015ed8fed9b909c74bcd8e
tinusaran <114895238+tinusaran@users.noreply.github.com>
2023-12-19 22:50:00 +0530

    Add files via upload

---
+++ pracMultiTable.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class pracMultiTable{
+public static void main(String arg[]){
+Scanner s=new Scanner(System.in);
+int i,m,n,p;
+System.out.print("Enter a number:");
+m=s.nextInt();
+System.out.print("Enter the value of n :");
+n=s.nextInt();
+for(i=1;i<=n;i++){
+p=i*m;
+System.out.println(i+"x"+m+"="+p);}
+}
+}
+
+
+
